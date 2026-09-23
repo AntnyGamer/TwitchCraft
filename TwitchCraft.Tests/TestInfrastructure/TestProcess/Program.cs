b03499b595086b9d463203bb0b7c74004a1e5bcc
Anthony Bongiovanni <79328760+AntnyGamer@users.noreply.github.com>
2026-09-23 19:06:28 -0400

    Simulate unique read-only probe markers

--- TwitchCraft.Tests/TestInfrastructure/TestProcess/Program.cs
+++ TwitchCraft.Tests/TestInfrastructure/TestProcess/Program.cs
@@ -87,9 +87,9 @@ while (await Console.In.ReadLineAsync() is string line)
     if (!responsive)
         continue;
 
-    if (string.Equals(line, "difficulty", StringComparison.Ordinal))
+    if (line.StartsWith("data get storage twitchcraft:tc_probe_", StringComparison.Ordinal))
     {
-        await WriteOutputAsync("The difficulty is normal");
+        await WriteOutputAsync("Storage " + line["data get storage ".Length..] + " has the following contents: {}");
         continue;
     }
 
