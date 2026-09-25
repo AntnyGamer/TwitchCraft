2ebe4b76b740fc9c39d6a5f76a36fb09aeeb1575
Anthony Bongiovanni <79328760+AntnyGamer@users.noreply.github.com>
2026-09-25 19:07:16 -0400

    Remove obsolete sidebar cleanup state reset

--- TwitchCraft_SOURCE/Runtime/Minecraft/ServerSession.cs
+++ TwitchCraft_SOURCE/Runtime/Minecraft/ServerSession.cs
@@ -34,7 +34,6 @@ public sealed partial class MainHandler
             _knownPlayers = [];
             _lastSidebarPlayers = [];
             _playerSidebarInitialized = false;
-            _playerSidebarCleared = false;
             Volatile.Write(ref _lastOnlinePlayersSnapshotTicks, 0);
         }
 
